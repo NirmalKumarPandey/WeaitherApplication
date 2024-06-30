540a9811314a2b1db7f112d9219684cf4a4057e1
NirmalKumarPandey <nirmalnirman36@gmail.com>
2024-06-30 19:15:36 +0530

    View Binding Implementation

--- app/src/main/java/com/example/weaitherapplication/MainActivity.kt
+++ app/src/main/java/com/example/weaitherapplication/MainActivity.kt
@@ -1,14 +1,9 @@
 package com.example.weaitherapplication
 
 
-import android.annotation.SuppressLint
 import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
-
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.weaitherapplication.Network.WeatherApplication
